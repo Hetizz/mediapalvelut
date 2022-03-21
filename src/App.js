9b4c330f548690b8e69189aa9988c1c6b2b2d575
Hetizz <heta.huostila@metropolia.fi>
2022-03-21 10:10:43 +0200

    navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
-import MediaTable from './components/MediaTable';
+// import MediaTable from './components/MediaTable';
+import Nav from './components/Nav';
+import {
+  BrowserRouter as Router, Route, Routes,
+} from 'react-router-dom';
+import Home from './views/Home';
+import Profile from './views/Profile';
 
 
 function App() {
-  return <MediaTable />;
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
