59c952e1a49249331670bfba7ca92e1ecfdc37bd
Hetizz <heta.huostila@metropolia.fi>
2022-03-25 13:44:48 +0200

    login part 2

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,31 @@
+import {useForm} from '../hooks/FormHooks';
+
 const LoginForm = (props) => {
-  return <div>LoginForm</div>;
+  const alkuarvot = {
+    username: '',
+    password: '',
+  };
+  const doLogin = () => {
+    console.log('doRegister');
+  };
+  const {inputs, handleInputChange} = useForm(doLogin, alkuarvot);
+  console.log(inputs);
+  return (
+    <form>
+      <input
+        placeholder="username"
+        name="username"
+        onChange={handleInputChange}
+      />
+      <input
+        placeholder="password"
+        name="password"
+        type="password"
+        onChange={handleInputChange}
+      />
+      <input type="submit" value="login" />
+    </form>
+  );
 };
 
 export default LoginForm;
